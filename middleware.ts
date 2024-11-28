f466a98c104c2c90649eb0b7e1d8a2a9631e7a2d
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-11-28 11:56:31 +0530

    Add middleware to handle the cors policy

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server';
+
+export function middleware() {
+  // Create a response
+  const response = NextResponse.next();
+
+  // Add CORS headers to all responses
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+  response.headers.set(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization'
+  );
+
+  return response;
+}
+
+export const config = {
+  matcher: '/api/:path*',
+};
